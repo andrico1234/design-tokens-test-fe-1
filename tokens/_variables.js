e83ed803c1797f9060da7374ced7ce192b1ab24b
andrico1234 <17087167+andrico1234@users.noreply.github.com>
2021-10-28 20:14:36 +0000

    Update platform deliverable

--- tokens/_variables.js
+++ tokens/_variables.js
@@ -1,11 +1,11 @@
 /**
  * Do not edit directly
- * Generated on Thu, 28 Oct 2021 20:08:42 GMT
+ * Generated on Thu, 28 Oct 2021 20:14:12 GMT
  */
 
 export const ColorWhite = "#ffffff";
 export const ColorBluePrimary = "#475a79";
-export const ColorBlueDark = "#aaaaae";
+export const ColorBlueDark = "#1c2f45";
 export const ColorBlueHover = "#26405d";
 export const ColorRedPrimary = "#f4616b";
 export const ColorRedDark = "#df5a63";
