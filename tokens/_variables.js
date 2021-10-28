d71f46ccf8c49043b447f84d4939c2734fe03d43
andrico1234 <17087167+andrico1234@users.noreply.github.com>
2021-10-28 19:20:55 +0000

    Update platform deliverable

---
+++ tokens/_variables.js
@@ -0,0 +1,33 @@
+/**
+ * Do not edit directly
+ * Generated on Thu, 28 Oct 2021 19:20:15 GMT
+ */
+
+export const ColorWhite = "#ffffff";
+export const ColorBluePrimary = "#475a79";
+export const ColorBlueDark = "#1c2f45";
+export const ColorBlueHover = "#26405d";
+export const ColorRedPrimary = "#f4616b";
+export const ColorRedDark = "#df5a63";
+export const ColorMintPrimary = "#8cdbc9";
+export const ColorGreyLight = "#efeded";
+export const ColorGreyDark = "#979797";
+export const FontDesktopH1 = {"fontSize":70,"textDecoration":"none","fontFamily":"Roboto","fontWeight":700,"fontStyle":"normal","fontStretch":"normal","letterSpacing":-2.1,"lineHeight":70,"paragraphIndent":0,"paragraphSpacing":0,"textCase":"none"};
+export const FontDesktopH2 = {"fontSize":56,"textDecoration":"none","fontFamily":"Roboto","fontWeight":700,"fontStyle":"normal","fontStretch":"normal","letterSpacing":-1.68,"lineHeight":62,"paragraphIndent":0,"paragraphSpacing":0,"textCase":"none"};
+export const FontDesktopH3 = {"fontSize":28,"textDecoration":"none","fontFamily":"Roboto","fontWeight":700,"fontStyle":"normal","fontStretch":"normal","letterSpacing":0.28,"lineHeight":32,"paragraphIndent":0,"paragraphSpacing":0,"textCase":"none"};
+export const FontDesktopLink = {"fontSize":18,"textDecoration":"none","fontFamily":"Roboto","fontWeight":700,"fontStyle":"normal","fontStretch":"normal","letterSpacing":0.9,"lineHeight":24,"paragraphIndent":0,"paragraphSpacing":0,"textCase":"none"};
+export const FontDesktopLabel = {"fontSize":16,"textDecoration":"none","fontFamily":"Roboto","fontWeight":400,"fontStyle":"normal","fontStretch":"normal","letterSpacing":0.8,"lineHeight":24,"paragraphIndent":0,"paragraphSpacing":0,"textCase":"uppercase"};
+export const FontDesktopH4Bold = {"fontSize":20,"textDecoration":"none","fontFamily":"Roboto","fontWeight":700,"fontStyle":"normal","fontStretch":"normal","letterSpacing":0.8,"lineHeight":30,"paragraphIndent":0,"paragraphSpacing":0,"textCase":"none"};
+export const FontDesktopH4Medium = {"fontSize":20,"textDecoration":"none","fontFamily":"Roboto","fontWeight":500,"fontStyle":"normal","fontStretch":"normal","letterSpacing":1,"lineHeight":30,"paragraphIndent":0,"paragraphSpacing":0,"textCase":"none"};
+export const FontDesktopH4Regular = {"fontSize":20,"textDecoration":"none","fontFamily":"Roboto","fontWeight":400,"fontStyle":"normal","fontStretch":"normal","letterSpacing":1,"lineHeight":30,"paragraphIndent":0,"paragraphSpacing":0,"textCase":"none"};
+export const FontDesktopBodyBold = {"fontSize":16,"textDecoration":"none","fontFamily":"Roboto","fontWeight":700,"fontStyle":"normal","fontStretch":"normal","letterSpacing":0.64,"lineHeight":26,"paragraphIndent":0,"paragraphSpacing":0,"textCase":"none"};
+export const FontDesktopBodyRegular = {"fontSize":16,"textDecoration":"none","fontFamily":"Roboto","fontWeight":400,"fontStyle":"normal","fontStretch":"normal","letterSpacing":0.8,"lineHeight":26,"paragraphIndent":0,"paragraphSpacing":0,"textCase":"none"};
+export const EffectShadow10 = {"shadowType":"dropShadow","radius":2,"color":"#00000033","offsetX":0,"offsetY":2,"spread":-1};
+export const EffectShadow11 = {"shadowType":"dropShadow","radius":1,"color":"#00000024","offsetX":0,"offsetY":1,"spread":0};
+export const EffectShadow12 = {"shadowType":"dropShadow","radius":3,"color":"#0000001f","offsetX":0,"offsetY":1,"spread":0};
+export const EffectShadow20 = {"shadowType":"dropShadow","radius":1,"color":"#00000033","offsetX":0,"offsetY":3,"spread":-2};
+export const EffectShadow21 = {"shadowType":"dropShadow","radius":2,"color":"#00000024","offsetX":0,"offsetY":2,"spread":0};
+export const EffectShadow22 = {"shadowType":"dropShadow","radius":5,"color":"#0000001f","offsetX":0,"offsetY":1,"spread":0};
+export const EffectShadow30 = {"shadowType":"dropShadow","radius":8,"color":"#0000001f","offsetX":0,"offsetY":1,"spread":0};
+export const EffectShadow31 = {"shadowType":"dropShadow","radius":4,"color":"#00000024","offsetX":0,"offsetY":3,"spread":0};
+export const EffectShadow32 = {"shadowType":"dropShadow","radius":3,"color":"#00000033","offsetX":0,"offsetY":3,"spread":-2};
